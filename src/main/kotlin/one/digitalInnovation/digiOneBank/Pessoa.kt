efbd96aed820fdd155290f9af7d120d50cf225e3
Bruno Salles <brunosalles1196@gmail.com>
2022-06-03 02:54:42 -0300

    Updated to talk about class members

--- src/main/kotlin/one/digitalInnovation/digiOneBank/Pessoa.kt
+++ src/main/kotlin/one/digitalInnovation/digiOneBank/Pessoa.kt
@@ -4,12 +4,13 @@ class Pessoa {
     var nome: String = "Bruno Salles"
     var cpf: String = "123.456.789-10"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome \n$cpf"
 }
 
 fun main() {
     val bruno = Pessoa()
-
-    println(bruno.nome)
-    println(bruno.cpf)
-
+    println(bruno.pessoaInfo())
 }
