63b01afec50e9a00b9dea09d1f98be3c25f0b10f
Bruno Salles <brunosalles1196@gmail.com>
2022-06-03 02:22:40 -0300

    Updated to apply the private modifier to set function of "cpf" property

--- src/main/kotlin/one/digitalInnovation/digiOneBank/Pessoa.kt
+++ src/main/kotlin/one/digitalInnovation/digiOneBank/Pessoa.kt
@@ -3,10 +3,13 @@ package one.digitalInnovation.digiOneBank
 class Pessoa {
     var nome: String = "Bruno Salles"
     var cpf: String = "123.456.789-10"
+    private set
 }
 
 fun main() {
     val bruno = Pessoa()
+
     println(bruno.nome)
     println(bruno.cpf)
+
 }
