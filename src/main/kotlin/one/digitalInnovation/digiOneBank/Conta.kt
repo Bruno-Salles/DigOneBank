002edb50d9976a0be3225c2600ff93bb22942e04
Bruno Salles <brunosalles1196@gmail.com>
2022-06-03 03:04:24 -0300

    Created account class

---
+++ src/main/kotlin/one/digitalInnovation/digiOneBank/Conta.kt
@@ -0,0 +1,17 @@
+package one.digitalInnovation.digiOneBank
+
+import java.math.BigDecimal
+
+class Conta(
+    val agencia: String,
+    val numConta: String,
+    val saldo: BigDecimal,
+) {
+    fun deposito(valor: BigDecimal) {
+
+    }
+
+    fun saque(valor: BigDecimal) {
+
+    }
+}
